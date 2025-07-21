011f584a5fc218c65eed15f24dd395044e1ea918
inhz <47627371+inhz@users.noreply.github.com>
2025-07-21 12:32:20 +0800

    change author

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://cry.cc/', // Site domain
-    title: '西墙序', // Site title
-    author: '芹溪散人', // Author name
-    description: '庞贝册为我的封地时庞贝已是废墟', // Site description
+    title: '西墙', // Site title
+    author: '芹溪客', // Author name
+    description: '庞贝册为我的封地时/庞贝已是废墟', // Site description
     language: 'zh-CN' // Default language
   },
 
